c00b464bd76f31e7d004ca9760d2f8eb109edc60
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-02-05 12:19:46 -0500

    floor added

--- flappy_bird_PyCharm_Project/main.py
+++ flappy_bird_PyCharm_Project/main.py
@@ -5,9 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((576, 1024))  # the canvas is going to be 576 x 1024
 clock = pygame.time.Clock()  # to be able to limit framerate
 
-bg_surface = pygame.image.load("assets/sprites/background-day.png")
+# loading the background image
+# convert()  -> it converts the image to a type of file that it's easier for pygame
+bg_surface = pygame.image.load("assets/sprites/background-day.png").convert()
 # pygame.transform.scale2x(picture)  -> to scale a picture 2x (doubling its size)
 bg_surface = pygame.transform.scale2x(bg_surface)
+
+floor_surface = pygame.image.load("assets/sprites/base.png").convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
 
 while True:
 
@@ -18,6 +23,7 @@ while True:
 
     # screen.blit(): to put 1 surface on top of the other one
     screen.blit(bg_surface, (0, 0))  # (0, 0) -> top left of the screen
+    screen.blit(floor_surface, (0, 900))
 
     pygame.display.update()  # it updates the screen
     clock.tick(120)  # to limit the framerate to 120, so the loop never runs faster than 120 fps
