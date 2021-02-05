c222870758723d32b9c40100b7689b35618208e2
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-02-05 11:21:11 -0500

    Basic setup for pygame added

---
+++ flappy_bird_PyCharm_Project/main.py
@@ -0,0 +1,18 @@
+
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((576, 1024))  # the canvas is going to be 576 x 1024
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+
+
+
+
